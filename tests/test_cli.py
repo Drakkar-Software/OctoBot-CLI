1de5f053b526b32493c2423c729fdba2373970b6
herklos <herklos.dev@protonmail.com>
2020-05-15 20:50:11 +0200

    [CI] Add travis

---
+++ tests/test_cli.py
@@ -0,0 +1,22 @@
+#  Drakkar-Software OctoBot-CLI
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+from multiprocessing.dummy import Namespace
+
+from octobot_cli.octobot_cli import manage_cli
+
+
+def test_manage_cli():
+    manage_cli(Namespace(version=True, update=False, verbose=True))
